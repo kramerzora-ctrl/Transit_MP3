c31f866745bd62abf3405b408cf82a6ed7f43bf9
kramerzora-ctrl <kramerzora@berkeley.edu>
2026-04-20 15:58:31 -0700

    New schedules

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from cumulative_curves import i_o
+from PartA import i_o
 
 # 2. Load data
-df = pd.read_csv('18_MF_NB_Test.csv')
+df = pd.read_csv('MP3 Starter Code/MP3_52 copy.csv')
+df['Input_SB'] = pd.to_datetime(df['Input_SB'], format='%I:%M %p')
+df = df.sort_values(by='Input_SB').reset_index(drop=True)
+df = df.dropna(subset=['Input_SB', 'Output_SB'])
 
 # 3. Call the function directly
-max_q, max_time = i_o(df)
+max_q, max_time, peak_info, df_final = i_o(df, input_col='Input_SB', output_col='Output_SB', title='52 Southbound', in_label='El Cerrito BART', out_label='Bancroft & Telegraph')
 
-print(f"Analysis Complete. Max Queue: {max_q}")
+durations = df_final['Output_SB'] - df_final['Input_SB']
+total_timedelta = durations.sum()
+
+# 3. Convert to a readable number (Total Hours as a float)
+total_hours = total_timedelta.total_seconds() / 3600
+
+
+
+print(f"Total Bus Hours: {total_hours:.2f}")
+print(f"Analysis Complete. Max Busses: {max_q} at time: {max_time} for period: {peak_info}")
